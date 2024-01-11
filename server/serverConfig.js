eb5a4b79acc75a42841ccad2994c85f57d64e5fb
OscarSerna <sernahernandezoscargilberto@gmail.com>
2024-01-11 17:12:40 -0600

    Segundo commit

--- server/serverConfig.js
+++ server/serverConfig.js
@@ -1,4 +1,4 @@
-export const PORT = 4000;
+export const PORT = process.env.PORT || 4000;
 export const reactURL = "http://localhost:5173";
 export const reactURLInternet = "https://statuesque-narwhal-59defa.netlify.app";
 export const directoryPath = "C:/Users/serna/OneDrive/Escritorio/mejor-software/server"
