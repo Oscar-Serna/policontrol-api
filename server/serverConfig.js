5d3d9acda1508f741baa7ef4d8b50912a176d01c
OscarSerna <sernahernandezoscargilberto@gmail.com>
2024-01-11 20:30:27 -0600

    Se quita la diagonal de la ruta oficial

--- server/serverConfig.js
+++ server/serverConfig.js
@@ -7,5 +7,5 @@ export const DB_NAME = process.env.DB_NAME || 'railway';
 export const DB_PORT = process.env.DB_PORT || 3306;
 
 export const reactURL = "http://localhost:5173";
-export const reactURLInternet = "https://policontrol.netlify.app/";
+export const reactURLInternet = "https://policontrol.netlify.app";
 export const directoryPath = "C:/Users/serna/OneDrive/Escritorio/mejor-software/server"
