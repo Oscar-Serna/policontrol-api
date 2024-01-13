0ef73ab4e583d307e2030aa8561eb722c2ca84c4
OscarSerna <sernahernandezoscargilberto@gmail.com>
2024-01-12 18:37:56 -0600

    Se agrega comentario a serverconfig.js

--- server/serverConfig.js
+++ server/serverConfig.js
@@ -6,6 +6,7 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || '';
 export const DB_NAME = process.env.DB_NAME || 'railway';
 export const DB_PORT = process.env.DB_PORT || 3306;
 
+// COMENTARIo
 export const reactURL = "http://localhost:5173";
 export const reactURLInternet = "https://policontrol.netlify.app";
 export const directoryPath = "C:/Users/serna/OneDrive/Escritorio/mejor-software/server"
